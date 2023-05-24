7f23e7a7a7cf658d63e16149b853443d79d308a6
abugabalgoal <120489382+abugabalgoal@users.noreply.github.com>
2023-05-24 22:22:16 +0200

    Update pid.py with correct values

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -35,9 +35,9 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 0
-        self.Ki = 0
-        self.Kd = 0
+        self.Kp = 18
+        self.Ki = 0.3
+        self.Kd = 0.1
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
@@ -53,6 +53,12 @@ class PIDController(object):
         @return control signal
         '''
         # YOUR CODE HERE
+        err = target-sensor
+        self.u = self.u +(self.Kp+self.Ki*self.dt+(self.Kd/self.dt))*err-(self.Kp+2*self.Kd/self.dt)*self.e1+self.Kd/self.dt*self.e2
+        self.e2 = self.e1
+        self.e1 = err
+
+        self.y.append(sensor+self.u*self.dt)
 
         return self.u
 
