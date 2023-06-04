da925c00f302adcfed7bd234663c9ec492d58b1b
abugabalgoal <120489382+abugabalgoal@users.noreply.github.com>
2023-06-04 21:08:41 +0200

    Update agent_server.py

--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -14,6 +14,8 @@
 # add PYTHONPATH
 import os
 import sys
+import time
+import numpy as np
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
 
 from inverse_kinematics import InverseKinematicsAgent
@@ -23,35 +25,52 @@ class ServerAgent(InverseKinematicsAgent):
     '''ServerAgent provides RPC service
     '''
     # YOUR CODE HERE
-    
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        return self.perception.joint[joint_name]
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        self.target_joints[joint_name] = angle
+        return (joint_name, " angle set to: ", str(angle))
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.recognize_posture(self.perception)
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        self.keyframes = keyframes
+        self.angle_interpolation(keyframes, self.perception)
+        sum = keyframes[1]
+        timing_now,starting_time = time.time()
+        finish = False
+        while not finish:
+            timing_now = time.time()
+            finish = True
+            for index in range(len(sum)):
+                if ((timing_now - starting_time) < sum[index][-1]):
+                    finish = False
+        return True
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        return self.transforms[name].tolist()
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        return self.set_transforms(effector_name, np.asarray(transform))
 
 if __name__ == '__main__':
     agent = ServerAgent()
